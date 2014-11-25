bf2224cdf8390f2193ede50221e88b8e9878174f
Edward Schults <edward.schults@gmail.com>
2014-11-25 19:04:04 +0100

    Tasks Manager

---
+++ db/migrate/20141125170031_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :description
+      t.integer :priority
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
